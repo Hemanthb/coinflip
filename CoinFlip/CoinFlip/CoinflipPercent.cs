59e0fb881aa7408b1c077be2bf135deebc28d708
hem_b <hemanthb93@gmail.com>
2022-05-16 06:15:56 +0530

    [hemanth]:coin flip percent calculation program

---
+++ CoinFlip/CoinFlip/CoinflipPercent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    internal class CoinflipPercent
+    {
+        int numOfTimes;
+        int tailsCount=0;
+        int headsCount=0;
+        double tailsPercent;
+        double headsPercent;
+        public void GetNumOfTimes()
+        {
+            Console.WriteLine("Enter the number of times to toss coin");
+            numOfTimes = Convert.ToInt32(Console.ReadLine());
+        }
+        public void CalculatePercentage()
+        {
+            Random random = new Random();
+            for(int i = 0; i < numOfTimes; i++)
+            {
+                double choice = random.NextDouble();
+                
+                if(choice > 0.5)
+                {
+                    tailsCount = tailsCount + 1;
+                    //Console.WriteLine(tailsCount);
+                }
+                else
+                {
+                    headsCount = headsCount + 1;
+                }
+
+            }
+            tailsPercent = (tailsCount*100)/numOfTimes;
+            headsPercent = (headsCount*100)/numOfTimes;
+            
+        }
+        public void displayPercent()
+        {
+            Console.WriteLine("Percentage of tails is" + tailsPercent+"%");
+            Console.WriteLine("Percentage of Heads is" + headsPercent+"%");
+        }
+    }
+}
---
+++ CoinFlip/CoinFlip/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+CoinFlip.CoinflipPercent coinflip = new CoinFlip.CoinflipPercent();
+coinflip.GetNumOfTimes();
+coinflip.CalculatePercentage();
+coinflip.displayPercent();
